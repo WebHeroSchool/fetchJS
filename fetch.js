8c5345ba770c4865fdf35dde244ba3fb9a19ec97
Maria Kasatkina <56779635+MariaKasa@users.noreply.github.com>
2020-04-17 22:42:39 +0300

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,7 +1,7 @@
 
 
 
-fetch('https://page.com?username=${login}' )
+fetch(https://api.github.com/users/?username=${login}' )
 .then(res=>res.json())
 // .then(json=> console.log(json))
 .then(json => {
